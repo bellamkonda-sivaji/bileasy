69e7a57c780b5b0fab5687c2888077b67389e2d9
sivaji-bellamkonda <sivajibellamkonda99@gmail.com>
2022-02-19 17:30:06 +0530

    path change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express')
 const app=express()
 const port=process.env.PORT || 3000
 
-app.get('/',async(req,res)=>{
+app.get('/welcome',async(req,res)=>{
     res.send('I am a vey good boy')
 })
 
